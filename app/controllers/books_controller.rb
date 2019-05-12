274b4729a774cf03e1ef678f256d5e45aae769f5
杉山千優 <chihirosugiyama@sugiyamasenyuunoMacBook-Pro.local>
2019-05-12 17:24:48 +0900

    finish bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,47 @@
+class BooksController < ApplicationController
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def home
+  end
+
+  def index
+  	@books =Book.all.order(id: "DESC")
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+      render("/books")
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+  def update
+        book = Book.find(params[:id])
+        if book.update(book_params)
+        flash[:notice] = "Book was successfully updated."
+        redirect_to book_path(book.id)
+        else
+        render("/books/show")
+    end
+  end
+  def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
